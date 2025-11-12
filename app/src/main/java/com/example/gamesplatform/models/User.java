47d39bad65945d5bc7fc985c0182a537f28a2106
Cyber_Mamriot <mt0584411551@gmail.com>
2025-11-13 01:30:17 +0200

    home page d

--- app/src/main/java/com/example/gamesplatform/models/User.java
+++ app/src/main/java/com/example/gamesplatform/models/User.java
@@ -2,13 +2,95 @@ package com.example.gamesplatform.models;
 
 public class User {
     public String id;
-
     public String username;
     public String email;
     public String password;
-
     public boolean isAdmin;
-
     public int money;
     public int exp;
+
+    public User(String id, String username, String email, String password, boolean isAdmin, int money, int exp) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = isAdmin;
+        this.money = money;
+        this.exp = exp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", isAdmin=" + isAdmin +
+                ", money=" + money +
+                ", exp=" + exp +
+                '}';
+    }
+
+    public User() {
+
+    }
+
+
 }
