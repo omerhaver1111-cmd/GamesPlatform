567f3a987b9afc7dde63f44b32d99f63b6eac286
Cyber_Mamriot <mt0584411551@gmail.com>
2025-11-12 09:47:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gamesplatform/models/Coin.java
@@ -0,0 +1,9 @@
+package com.example.gamesplatform.models;
+
+public class Coin {
+    public String imgPath;
+    public int value;
+    public double minHeight;
+    public double maxHeight;
+
+}
---
+++ app/src/main/java/com/example/gamesplatform/models/User.java
@@ -0,0 +1,14 @@
+package com.example.gamesplatform.models;
+
+public class User {
+    public String id;
+
+    public String username;
+    public String email;
+    public String password;
+
+    public boolean isAdmin;
+
+    public int money;
+    public int exp;
+}
